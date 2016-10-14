29bec9a77333dcde71bbe265dda61418cde057ef
liuchonghui <liuchonghui@xiaomi.com>
2016-10-14 10:38:34 +0800

    commit changes.
    N/A
    
    Signed-off-by: liuchonghui <liuchonghui@xiaomi.com>

--- ImageDownloader/samples/test/src/main/java/tool/imagedownloader/fragment/MainFragment.java
+++ ImageDownloader/samples/test/src/main/java/tool/imagedownloader/fragment/MainFragment.java
@@ -4,11 +4,14 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.File;
+import java.net.URISyntaxException;
 import java.net.URLEncoder;
 
 import tool.imagedownloader.activity.FeiDianActivity;
@@ -81,6 +84,7 @@ public class MainFragment extends BaseFragment {
                 intent.putExtra("royalMessages", "T01020978");
                 intent.putExtra("from", getActivity().getPackageName()); // 数据统计用
                 intent.setData(Uri.parse("mifg://fashiongallery/push_preview"));
+                Log.d("DIAOQI", "strintent=" + intent.toUri(Intent.URI_INTENT_SCHEME));
                 getActivity().startActivity(intent);
             }
         });
@@ -90,6 +94,7 @@ public class MainFragment extends BaseFragment {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 String url = "http://video.mi.com/player?id=V001338875";
                 intent.setData(Uri.parse(url));
+                Log.d("DIAOQI", "strintent=" + intent.toUri(Intent.URI_INTENT_SCHEME));
                 getActivity().startActivity(intent);
             }
         });
@@ -101,6 +106,7 @@ public class MainFragment extends BaseFragment {
                 intent.putExtra("from", getActivity().getPackageName()); // 数据统计打点用
                 intent.setData(Uri.parse(
                         "mifg://fashiongallery/express_preview")); // 写死
+                Log.d("DIAOQI", "strintent=" + intent.toUri(Intent.URI_INTENT_SCHEME));
                 getActivity().startActivity(intent);
             }
         });
@@ -116,6 +122,7 @@ public class MainFragment extends BaseFragment {
                 sb.append("&from=").append(getActivity().getPackageName());
                 sb.append("&id=").append("T01015354");
                 intent.setData(Uri.parse(sb.toString()));
+                Log.d("DIAOQI", "strintent=" + intent.toUri(Intent.URI_INTENT_SCHEME));
                 getActivity().startActivity(intent);
             }
         });
@@ -130,6 +137,74 @@ public class MainFragment extends BaseFragment {
                 sb.append("mifb=").append(encodeUrl);
                 sb.append("&from=").append(getActivity().getPackageName());
                 sb.append("&id=").append("T01015354");
+                intent.setData(Uri.parse(sb.toString()));
+                Log.d("DIAOQI", "strintent=" + intent.toUri(Intent.URI_INTENT_SCHEME));
+                getActivity().startActivity(intent);
+            }
+        });
+        view.findViewById(R.id.testweb).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                StringBuilder sb = new StringBuilder();
+                sb.append("mifg://fashiongallery/h5?");
+                sb.append("from=").append("com.mfashiongallery.emag");
+                String url = "http://www.baidu.com";
+                sb.append("&url=").append(url);
+                intent.setData(Uri.parse(sb.toString()));
+                Log.d("DIAOQI", "strintent=" + intent.toUri(Intent.URI_INTENT_SCHEME));
+                getActivity().startActivity(intent);
+            }
+        });
+        view.findViewById(R.id.testweb2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                String strintent = "intent://fashiongallery/h5?from=com.mfashiongallery.emag&url=http://www.baidu.com#Intent;scheme=mifg;end";
+                try {
+                    intent = Intent.parseUri(strintent, Intent.URI_INTENT_SCHEME);
+                    Log.d("DIAOQI", "strintent=" + intent.toUri(Intent.URI_INTENT_SCHEME));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                getActivity().startActivity(intent);
+            }
+        });
+        view.findViewById(R.id.testvideo1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.miui.videoplayer.LOCAL_VIDEO_PLAY");
+                intent.setData(Uri.fromFile(new File("/mnt/sdcard/video_guanggao.mp4")));
+                intent.putExtra("StartActivityWhenLocked", true);
+                Log.d("DIAOQI", "strintent=" + intent.toUri(Intent.URI_INTENT_SCHEME));
+                getActivity().startActivity(intent);
+            }
+        });
+        view.findViewById(R.id.testvideo2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                String playUrl = "http://image.cdn.mvideo.xiaomi.com/mfsv2/download/fdsc3/p01InEmhXMlU/DTqPYfUemVAl1h.mp4";
+                String encodeUrl = URLEncoder.encode(playUrl);
+                StringBuilder sb = new StringBuilder();
+                sb.append("mivideo://video/play_lock?");
+                sb.append("id=").append(playUrl);
+                intent.setData(Uri.parse(sb.toString()));
+                Log.d("DIAOQI", "strintent=" + intent.toUri(Intent.URI_INTENT_SCHEME));
+                getActivity().startActivity(intent);
+            }
+        });
+        view.findViewById(R.id.testvideo3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                String playUrl = "http://image.cdn.mvideo.xiaomi.com/mfsv2/download/fdsc3/p01InEmhXMlU/DTqPYfUemVAl1h.mp4";
+                String encodeUrl = URLEncoder.encode(playUrl);
+                StringBuilder sb = new StringBuilder();
+                sb.append("mivideo://video/play_lock?");
+                sb.append("StartActivityWhenLocked=").append(true);
+                sb.append("&from=").append("com.mfashiongallery.emag");
+                sb.append("&id=").append(encodeUrl);
                 intent.setData(Uri.parse(sb.toString()));
                 getActivity().startActivity(intent);
             }
